539761517b565e550fe41312947d45d685a3f073
Miguel-Huayhua <78826211+Miguel-Huayhua@users.noreply.github.com>
2022-02-24 00:42:41 -0400

    tercero

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const fluent = require('fluent-ffmpeg')
 
 app.get('/', (req, res) => {
 
-    fluent('prueba.mp4').toFormat('mp3').audioBitrate('128k').audioCodec('libmp3lame').save('./prueba.mp3')
-    res.download('prueba.mp3')
+    fluent('prueba.mp4').toFormat('mp3').audioBitrate('128k').audioCodec('libmp3lame').save('./prueba.mp3').on('end',()=>{
+        res.download('prueba.mp3')
+
+    })
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
