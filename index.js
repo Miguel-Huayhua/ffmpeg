aedadaed6422239b6cc0a92a3b6010bb25527b6c
Miguel-Huayhua <78826211+Miguel-Huayhua@users.noreply.github.com>
2022-02-24 00:30:03 -0400

    segundo

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+const fluent = require('fluent-ffmpeg')
+
+
+app.get('/', (req, res) => {
+
+    fluent('prueba.mp4').toFormat('mp3').audioBitrate('128k').audioCodec('libmp3lame').save('./prueba.mp3')
+    res.download('prueba.mp3')
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
